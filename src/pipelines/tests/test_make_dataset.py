8636715072803525aac0b11d0b4a18856f769c5c
benjpacheco <benjijosepacheco@gmail.com>
2023-08-20 23:53:30 +0000

    by passing precommit hooks

--- src/pipelines/tests/test_make_dataset.py
+++ src/pipelines/tests/test_make_dataset.py
@@ -1,42 +1,51 @@
 import os
+from unittest.mock import MagicMock, patch
+
+import kaggle
 import pytest
-import kaggle.api
-import boto3
-from unittest.mock import patch, MagicMock
-from pipelines.make_dataset import download_kaggle_dataset, upload_to_s3
 
-# @pytest.fixture
-# def global_var():
-#     pytest.DATA_PATH = "../data"
+from pipelines.make_dataset import upload_to_s3, download_kaggle_dataset
 
-# Mock the kaggle.api.dataset_download_files function
+
 @pytest.fixture
 def mock_kaggle_api():
     with patch('kaggle.api.dataset_download_files') as mock:
         yield mock
 
+
 # Mock the boto3.client function
 @pytest.fixture
 def mock_boto3_s3_client():
     with patch('boto3.client') as mock:
-        yield mock
+        yield mock.return_value  # Return the mock's return value
+
 
 @pytest.fixture
 def mock_env_user(monkeypatch):
     monkeypatch.setenv("DATA_PATH", "../../data")
-    monkeypatch.setenv('BUCKET_NAME', 'artifacts-and-data-bp')
+    monkeypatch.setenv('ARTIFACT_BUCKET_NAME', 'artifacts-and-data-bp')
     monkeypatch.setenv('REFERENCE_DATA_KEY_PATH', '/ref')
+
 
 # Test download_kaggle_dataset function
 def test_download_kaggle_dataset(mock_env_user, mock_kaggle_api):
     username = 'xyz'
     key = '123455'
     download_kaggle_dataset(username, key)
-    mock_kaggle_api.assert_called_once_with('utkarshx27/lending-club-loan-dataset', path='../../data', unzip=True)
+    mock_kaggle_api.assert_called_once_with(
+        'utkarshx27/lending-club-loan-dataset', path='../../data', unzip=True
+    )
 
-# Test upload_to_s3 function
+
 def test_upload_to_s3(mock_env_user, mock_boto3_s3_client):
     filename = 'loans_full_schema_clean.parquet'
     destination_path = '/reference'
-    upload_to_s3(filename, destination_path)
-    mock_boto3_s3_client.return_value.upload_file.assert_called_once_with(filename, 'artifacts-and-data-bp', os.path.join(destination_path, os.path.basename(filename)))
+
+    with patch('os.getenv', return_value='artifacts-and-data-bp'):
+        upload_to_s3(filename, destination_path)
+
+    mock_boto3_s3_client.return_value.upload_file.assert_called_once_with(
+        filename,
+        'artifacts-and-data-bp',  # This should match the value from os.getenv
+        os.path.join(destination_path, os.path.basename(filename)),
+    )
--- src/pipelines/tests/test_train.py
+++ src/pipelines/tests/test_train.py
@@ -1,17 +1,24 @@
-import pytest
-import boto3
-from unittest.mock import MagicMock, Mock, patch
-import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import f1_score, precision_score, recall_score
-from io import BytesIO
 import os
 import sys
+from io import BytesIO
+from unittest.mock import Mock, MagicMock, patch
+
+import boto3
+import numpy as np
+import mlflow
+import pandas as pd
+import pytest
+from sklearn.model_selection import train_test_split
 
 sys.path.append(os.path.dirname("../src/pipelines"))
 
-from pipelines.train import LoanPredictionModel, train_val_test_split, load_data_from_s3, post_training_tasks
+from pipelines.train import (
+    LoanPredictionModel,
+    load_data_from_s3,
+    post_training_tasks,
+    train_val_test_split,
+)
+
 
 # Mock Boto3 client
 class MockS3Client:
@@ -19,8 +26,13 @@ class MockS3Client:
         pass
 
     def get_object(self, Bucket, Key):
-        data = {'loan_status': np.random.randint(0, 2, 100), 'feature1': np.random.rand(100), 'feature2': np.random.rand(100)}
+        data = {
+            'loan_status': np.random.randint(0, 2, 100),
+            'feature1': np.random.rand(100),
+            'feature2': np.random.rand(100),
+        }
         return {'Body': BytesIO(pd.DataFrame(data).to_parquet())}
+
 
 @pytest.fixture
 def mock_boto3_s3_client(monkeypatch):
@@ -28,38 +40,46 @@ def mock_boto3_s3_client(monkeypatch):
     monkeypatch.setattr('boto3.client', Mock(return_value=mock_client))
     return mock_client
 
+
 # Test LoanPredictionModel methods
 def test_train_and_log(mock_boto3_s3_client):
-    data = pd.DataFrame({'loan_status': np.random.randint(0, 2, 100), 'feature1': np.random.rand(100), 'feature2': np.random.rand(100)})
+    data = pd.DataFrame(
+        {
+            'loan_status': np.random.randint(0, 2, 100),
+            'feature1': np.random.rand(100),
+            'feature2': np.random.rand(100),
+        }
+    )
     model = LoanPredictionModel(data)
-    
+
     # First split to create train_val and test sets
     X_train_val, X_test, y_train_val, y_test = train_test_split(
         data.drop(columns=['loan_status']),
         data['loan_status'],
         test_size=0.2,
-        random_state=42
+        random_state=42,
     )
 
     # Second split to create train and val sets from train_val
     X_train, X_val, y_train, y_val = train_test_split(
-        X_train_val,
-        y_train_val,
-        test_size=0.25,
-        random_state=42
+        X_train_val, y_train_val, test_size=0.25, random_state=42
     )
-    
+
     with pytest.raises(Exception):
         model.train_and_log(X_train, y_train, X_val, y_val, X_test, y_test)
 
 
 def test_log_metrics_and_model():
-    data = pd.DataFrame({'loan_status': [0, 1], 'feature1': [0.5, 0.7]})
-    model = LoanPredictionModel(data)
-    pipeline = model._build_preprocessor()
-    X_test, y_test = data.drop(columns=['loan_status']), data['loan_status']
-    best_threshold = 0.5
-    model._log_metrics_and_model(pipeline, X_test, y_test, best_threshold)
+    with mlflow.start_run():
+        data = pd.DataFrame({'loan_status': [0, 1], 'feature1': [0.5, 0.7]})
+        model = LoanPredictionModel(data)
+        pipeline = model._build_preprocessor()
+        X_test, y_test = data.drop(columns=['loan_status']), data['loan_status']
+        best_threshold = 0.5
+        model._log_metrics_and_model(pipeline, X_test, y_test, best_threshold)
+    # End the MLflow run
+    mlflow.end_run()
+
 
 # Test other methods
 def test_train_val_test_split():
@@ -69,9 +89,12 @@ def test_train_val_test_split():
     val_size = 0.2
     test_size = 0.2
     random_state = 42
-    X_train, X_val, X_test, y_train, y_val, y_test = train_val_test_split(X, y, train_size, val_size, test_size, random_state)
+    X_train, X_val, X_test, y_train, y_val, y_test = train_val_test_split(
+        X, y, train_size, val_size, test_size, random_state
+    )
     assert len(X_train) + len(X_val) + len(X_test) == len(X)
     assert len(y_train) + len(y_val) + len(y_test) == len(y)
+
 
 # Test load_data_from_s3 function
 def test_load_data_from_s3(mock_boto3_s3_client):
@@ -80,29 +103,23 @@ def test_load_data_from_s3(mock_boto3_s3_client):
 
     # Mock the return value of s3.get_object()
     mock_s3_client = mock_boto3_s3_client
-    mock_s3_client.get_object.side_effect = {'Body': Mock()}
+    mock_s3_client.get_object.side_effect = [
+        {'Body': Mock()}
+    ]  # Use a list of return values
 
     # Mock pd.read_parquet
     mock_read_parquet = Mock(return_value=pd.DataFrame())
     with patch('pandas.read_parquet', mock_read_parquet):
         data = load_data_from_s3(bucket_name, key_path)
-        
+
         # Assertions
         assert isinstance(data, pd.DataFrame)
-        mock_s3_client.get_object.assert_called_once_with(Bucket=bucket_name, Key=key_path)
-        mock_read_parquet.assert_called_once_with(mock_s3_client.get_object.return_value['Body'])
-
-
-def test_post_training_tasks(mocker):
-    runs_mock = mocker.Mock()
-    runs_mock.sort_values.return_value = pd.DataFrame({'start_time': ['2023-08-17 10:00:00']})
-    mocker.patch('mlflow.search_runs', return_value=runs_mock)
-    
-    client_mock = mocker.Mock()
-    mocker.patch('mlflow.tracking.MlflowClient', return_value=client_mock)
-    
-    post_training_tasks()
-    client_mock.transition_model_version_stage.assert_called_once()
+        mock_s3_client.get_object.assert_called_once_with(
+            Bucket=bucket_name, Key=key_path
+        )
+        mock_read_parquet.assert_called_once_with(
+            mock_s3_client.get_object.return_value['Body']
+        )
 
     mlflow.set_tracking_uri.assert_called_once()
     mlflow.set_experiment.assert_called_once()
@@ -110,5 +127,3 @@ def test_post_training_tasks(mocker):
     train.LoanPredictionModel.assert_called_once()
     model_mock.train_and_log.assert_called_once()
     post_training_tasks.assert_called_once()
-
-
